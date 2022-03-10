8920cbba1107acb58f604cb14b2b6295d5902cc3
Kaue Lima <kaue.lima13@gmail.com>
2022-03-10 16:12:43 -0300

    minify getProjects response

--- pages/api/getProjects.js
+++ pages/api/getProjects.js
@@ -4,18 +4,33 @@ const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.e
 // Reference a table
 const table = base(process.env.AIRTABLE_TABLE_NAME);
 
-
-export default async function projectData (_req, res) {
+export default async function projectsData (_req, res) {
     const records = table.select({
-        // Selecting the first 10 records in Full Data:
-        maxRecords: 10,
-        view: "Full Data"
+    // Selecting the first 10 records in Full Data
+    maxRecords: 10,
+    view: "Full Data"
+
     }).firstPage(function(err, records) {
+        
         if (err) { console.error(err); return; }
         records.forEach(function(record) {
             console.log('Retrieved', record.get('title'));
         });
-        res.json(records);
+        // Push airtable response to endpoint response
+        
+
+        // To get minified records array
+        const minifyItems = (records) =>
+        records.map((record) => getMinifiedItem(record));
+
+        // to make record meaningful.
+        const getMinifiedItem = (record) => {
+            return {
+                id: record.id,
+                fields: record.fields,
+            };
+        };
+        res.json(minifyItems(records));
     });
     
 };
