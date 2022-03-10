4fc3a26861656aa89665b13b9d0c86a73d85edc4
Kaue Lima <kaue.lima13@gmail.com>
2022-03-10 13:33:58 -0300

    fix:getRecords response

--- pages/api/getProjects.js
+++ pages/api/getProjects.js
@@ -5,14 +5,17 @@ const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.e
 const table = base(process.env.AIRTABLE_TABLE_NAME);
 
 
-export default async (_req, res) => {
+export default async function (_req, res) {
     const records = table.select({
-        view: 'Full Data'
+        // Selecting the first 10 records in Full Data:
+        maxRecords: 10,
+        view: "Full Data"
     }).firstPage(function(err, records) {
         if (err) { console.error(err); return; }
         records.forEach(function(record) {
             console.log('Retrieved', record.get('title'));
-            res.json(records)
         });
+        res.json(records);
     });
+    
 };
