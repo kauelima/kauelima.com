1ba0707cf2cddf68fe22dc03edc2403753a23d4d
Kaue Lima <kaue.lima13@gmail.com>
2022-03-10 10:33:49 -0300

    fix:getProjects

--- pages/api/getProjects.js
+++ pages/api/getProjects.js
@@ -1,11 +1,18 @@
-import { table } from "../../utils/Airtable";
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
+
+// Reference a table
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+
 
 export default async (_req, res) => {
-  try {
-    const records = await table.select({view: "Full Data"}).firstPage();
-    res.status(200).json(records);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ msg: "Could not get data from Airtable" });
-  }
+    const records = table.select({
+        view: 'Full Data'
+    }).firstPage(function(err, records) {
+        if (err) { console.error(err); return; }
+        records.forEach(function(record) {
+            console.log('Retrieved', record.get('title'));
+            res.json(records)
+        });
+    });
 };
